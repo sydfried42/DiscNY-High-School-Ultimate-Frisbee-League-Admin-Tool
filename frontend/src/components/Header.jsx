9cb07d65479b5bfa85510246663b8893f9455eb5
sydneyfriedman <friedman.sydneyl@gmail.com>
2024-04-15 13:03:00 -0400

    added text and images to header and footer

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <img src="https://m.media-amazon.com/images/I/51Jv-3hJ0xL.__AC_SX300_SY300_QL70_FMwebp_.jpg" alt="nyc skyline"/>
+    </div>
   )
 }
 
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
         <p>Our league is sanctioned by USA Ultimate, meaning all teams, coaches, chaperones and individual players must be rostered through USAU to participate. Information on that process has been emailed to team leadership and posted on discny.org via our HSL admin team.</p>
         <p>Schedules and standings will all be posted on our event page here on this site, as well as rules on another tab. Contruction is still underway.</p>
         <p>temp space to add pictures?</p>
-        <h2>Further notes on this site</h2>
+        <h2>Further notes on this site:</h2>
         <p>Just like our programs, expansions to this website are ongoing. As we collect and organize more and more information, you can expect new pages and page networks to launch here. Currently, we're working on (re)building our resource pages for players, coaches, and parents, and a few other sets of pages we're hoping to launch by the end of the spring.</p>
         <p>We hope you'll consider <a href= "https://youth.discny.org/donate">donating to our youth programs</a>! We work hard to make our programs financially accessible for all participants, and your generous donations help us achieve that goal! Plus, as a 501(c)(3) nonprofit, all donations to DiscNY are tax-deductible.</p>
     </>

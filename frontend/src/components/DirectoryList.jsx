2686113692f523c398db3c1d314a3c94cbacf4cc
Stefan Pla <spla@icloud.com>
2024-04-17 14:44:40 -0400

    assembled a coach directory and a player directory at the directory route with some basic css

--- frontend/src/components/DirectoryList.jsx
+++ frontend/src/components/DirectoryList.jsx
@@ -1,33 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import DirectoryCoachItem from './DirectoryCoachItem.jsx';
 import DirectoryPlayerItem from './DirectoryPlayerItem.jsx';
 
 function DirectoryList() {
+
+  const [playerRows, setPlayerRows] = useState([])
+  const [coachRows, setCoachRows] = useState([])
+
+  useEffect(()=> {
+    fetch("http://127.0.0.1:5555/coaches")
+    .then(response=>response.json())
+    .then(data=>{
+      console.log(data)
+      setCoachRows(data)
+    })},
+  []);
+
+  useEffect(()=> {
+    fetch("http://127.0.0.1:5555/players")
+    .then(response=>response.json())
+    .then(data=>{
+      console.log(data)
+      setPlayerRows(data)
+    })},
+  []);
+
+
   return (
     <div>
-      <table>
-        <th>
-          Coaches
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Pronouns</th>
-          <th>USAU Number</th>
-          <th>Team Role</th>
-          <th>Team ID</th>
-        </th>
+      <table className="directory-table">
+        <p className="table-title">Coaches</p>
+        <tr className="directory-heading">
+          <th className="directory-heading">First Name</th>
+          <th className="directory-heading">Last Name</th>
+          <th className="directory-heading">Pronouns</th>
+          <th className="directory-heading">USAU Number</th>
+          <th className="directory-heading">Team Role</th>
+        </tr>{
+          coachRows.map((coachRow)=>
+          <DirectoryCoachItem 
+            key={coachRow.id}
+            id={coachRow.id}
+            first_name={coachRow.first_name}
+            last_name={coachRow.last_name}
+            pronouns={coachRow.pronouns}
+            usau={coachRow.usau}
+            team_role={coachRow.team_role}
+          />
+          )}
       </table>
-      <table>
-        <th>
-        Players
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Pronouns</th>
-          <th>USAU Number</th>
-          <th>Email</th>
-          <th>Birthday</th>
-          <th>Grade</th>
-          <th>Team ID</th>
-        </th>
+      <table className="directory-table">
+        <p className="table-title">Players</p>
+        <tr className="directory-heading">
+          <th className="directory-heading">First Name</th>
+          <th className="directory-heading">Last Name</th>
+          <th className="directory-heading">Pronouns</th>
+          <th className="directory-heading">USAU Number</th>
+          <th className="directory-heading">Email</th>
+          <th className="directory-heading">Birthday</th>
+          <th className="directory-heading">Grade</th>
+        </tr>{
+          playerRows.map((playerRow)=>
+          <DirectoryPlayerItem 
+            key={playerRow.id}
+            id={playerRow.id}
+            first_name={playerRow.first_name}
+            last_name={playerRow.last_name}
+            pronouns={playerRow.pronouns}
+            usau={playerRow.usau}
+            email={playerRow.email}
+            birthday={playerRow.birthday}
+            grade={playerRow.grade}
+          />
+          )}
       </table>
     </div>
   )

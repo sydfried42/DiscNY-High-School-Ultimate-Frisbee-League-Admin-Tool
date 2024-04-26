501d21f3462933248dc6e22882023b7f4fd05ec6
sydneyfriedman <friedman.sydneyl@gmail.com>
2024-04-26 10:40:26 -0400

    added to teams fetch to directory page

--- frontend/src/components/DirectoryPage.jsx
+++ frontend/src/components/DirectoryPage.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import TeamList from './TeamList';
 
 function DirectoryPage() {
   //move state and useEffects here, pass as props
 
+  const [teamRows, setTeamRows] = useState([])
+
+  useEffect(()=> {
+    fetch("http://127.0.0.1:5555/teams")
+    .then(response=>response.json())
+    .then(data=>{
+      console.log(data)
+      setTeamRows(data)
+    })},
+  []);
+
   return (
     <div>
-      <TeamList />
+      {teamRows.map((teamRow)=>
+      <TeamList 
+        key={teamRow.id}
+        id={teamRow.id}
+        name={teamRow.name}
+      />
+      )}
     </div>
   )
 }

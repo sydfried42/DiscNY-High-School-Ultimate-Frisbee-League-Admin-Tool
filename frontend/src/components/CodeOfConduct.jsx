c029722bbb5a3cc98802cab3a01d230418872c9b
sydneyfriedman <friedman.sydneyl@gmail.com>
2024-08-26 14:12:37 -0400

    updated code of conduct

--- frontend/src/components/CodeOfConduct.jsx
+++ frontend/src/components/CodeOfConduct.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
 
+/**
+ * CodeOfConduct Component
+ * This component displays a PDF document (Code of Conduct) embedded in the page.
+ * 
+ * It uses an <embed> tag to render the PDF directly in the browser.
+ */
 const CodeOfConduct = () => {
-  const pdfUrl = 'http://localhost:5173/HSLRules2024.pdf'
+  // URL of the PDF document to be displayed
+  const pdfUrl = 'http://localhost:5173/HSLRules2024.pdf';
 
   return (
-    <div class='body'>
-      <Typography className="centered-h1" variant="h1" gutterBottom style={{ marginBottom: '20px' }}>
-      Code of Conduct
+    <div className='body'>
+      {/* Header for the Code of Conduct section */}
+      <Typography
+        className="centered-h1"
+        variant="h1"
+        gutterBottom
+        style={{ marginBottom: '20px' }}
+      >
+        Code of Conduct
       </Typography>
+
+      {/* Container to embed the PDF document */}
       <div style={{ width: '100%', height: '800px' }}>
         <embed src={pdfUrl} type="application/pdf" width="100%" height="100%" />
       </div>
@@ -17,6 +32,3 @@ const CodeOfConduct = () => {
 };
 
 export default CodeOfConduct;
-
-
-
